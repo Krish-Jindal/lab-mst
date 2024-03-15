e6d71178e65d4c950556e5964873616e9be79e39
Krish Jindal <54677508+Krish-Jindal@users.noreply.github.com>
2024-03-15 10:20:20 +0530

    Update labmst1.py

--- labmst1.py
+++ labmst1.py
@@ -1,1 +1,10 @@
-print("\n\n\t\tKrish Jindal | 21BCS7871")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '\n\n\t\tKrish Jindal | 21BCS7871'
+
+if __name__ == '__main__':
+    app.run(debug=True)
